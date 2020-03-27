87805235596f1d7aeefc210ec7316dc9ba65f1fb
Marta <martaprotsak13@gmail.com>
2020-03-28 01:19:28 +0200

    Add usersWithAccess to TaskDto in Mapper

--- src/main/java/com/protsak/mapper/TaskMapper.java
+++ src/main/java/com/protsak/mapper/TaskMapper.java
@@ -25,6 +25,7 @@ public class TaskMapper {
                 .title(model.getTitle())
                 .description(model.getDescription())
                 .dateTime(model.getDateTime())
+                .usersWithAccess(model.getUsersWithAccess())
                 .build();
 
     }
